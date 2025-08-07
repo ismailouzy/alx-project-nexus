b775ace09494da63f11fbc7acb19572b18894965
ismailouzy <ismailouzal@gmail.com>
2025-08-07 15:48:32 +0100

    feat: create models for posts, comments, and interactions

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(
+            User, on_delete=models.CASCADE, related_name='posts')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'Post made by ' + self.author.username
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+            Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author.username} commented something"
+
+
+class Interaction(models.Model):
+    Interaction_type = models.CharField(
+            max_length=10, choices=[('LIKE', 'like'), ('SHARE', 'share')])
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.Interaction_type + "on post " + str(self.post.id)
