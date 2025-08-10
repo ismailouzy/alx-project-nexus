c92076c7042301d78e9a890ae2d188cfc9700b96
ismailouzy <ismailouzal@gmail.com>
2025-08-10 18:29:54 +0100

    fix: Configure Redis cache with Upstash via environment variable

--- socsyn/settings.py
+++ socsyn/settings.py
@@ -70,9 +70,10 @@ GRAPHENE = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.environ.get("REDIS_URL"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"ssl_cert_reqs": None},
         }
     }
 }
