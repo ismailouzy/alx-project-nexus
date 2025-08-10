0d8e9fcce163bfe4574ed15f491ec15388acd36e
ismailouzy <ismailouzal@gmail.com>
2025-08-10 16:49:13 +0100

    allowing hosts

--- socsyn/settings.py
+++ socsyn/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
