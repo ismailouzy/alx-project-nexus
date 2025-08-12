2eb046dda02aaeb140f211e45679e2b9d9688fa3
ismailouzy <ismailouzal@gmail.com>
2025-08-12 19:13:05 +0100

    fix(graphql): properly configure JWT auth in settings.py

--- socsyn/settings.py
+++ socsyn/settings.py
@@ -111,6 +111,12 @@ DATABASES = {
     }
 }
 
+
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
